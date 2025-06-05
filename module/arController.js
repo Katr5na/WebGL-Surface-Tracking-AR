ec0d6d1b38ce26fb3ac289a7424c3f6b0fb73b70
Katr5na <katrina.stryhun@adv.ua>
2025-06-05 17:30:49 +0300

    chore (changed connect variant) Update arController.js

--- module/arController.js
+++ module/arController.js
@@ -1,7 +1,7 @@
 // modules/ARController.js
-import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
-import { GLTFLoader } from 'https://unpkg.com/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
-import { ARButton } from 'https://unpkg.com/three@0.160.0/examples/jsm/webxr/ARButton.js';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { ARButton } from 'three/examples/jsm/webxr/ARButton.js';
 
 /**
  * ARController відповідає за:
